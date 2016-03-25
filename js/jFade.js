154d640e0598b60f6129fa0aaba3bb8da60ae051
Svyatoslav <kiev.devel@gmail.com>
2016-03-25 10:37:15 +0100

    LP

---
+++ js/jFade.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    var md = new MobileDetect(window.navigator.userAgent);
+
+    if ( ! md.mobile()) {
+
+        $('.fade-bottom').fadeThis({
+            speed: 1000
+        });
+
+        $('.first-screen-animation').animate({
+            bottom: "+=10vh",
+            opacity: 1
+        }, 1500);
+    }
+
+});
